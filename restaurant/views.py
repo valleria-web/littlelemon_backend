cf6ca1a02b87aeeeff7e2c8221a97228491b0f00
valleria-web <valleria@gmail.com>
2023-02-01 11:03:11 +0100

    ¨styling¨

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+def home(request):
+    return HttpResponse("<div style='background-color: yellow; text-align: center;'><h1 style='color: green; font-size: 100px; font-family: Arial'>Little Lemon is cooking <h2 style='color: orange; text-align: center; font-size: 90px;'>The dishes will be ready soon <h3 style='color: text-align: center; black; font-size: 50px; font-family: Arial'>Thank you for your patience :) </h2></div>")
