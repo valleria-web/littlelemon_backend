671c63745b6de3904e73cb12b958359265ecb1b1
valleria-web <valleria@gmail.com>
2023-02-02 23:27:33 +0100

    tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
-from . import Menu
-from . import MenuSerializer
+from .models import Menu
+from .serializers import MenuSerializer
 
 class MenuViewTest(APITestCase):
 
