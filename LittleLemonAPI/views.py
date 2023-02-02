9e2cda7a5ffcec11817bd4f3895359beb7d101b7
valleria-web <valleria@gmail.com>
2023-02-02 22:48:21 +0100

    LittleLemonAPI

---
+++ LittleLemonAPI/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+
+# Create your views here. 
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
